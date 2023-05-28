afd228cc49d0a722a81f884cb7406ab83549f5bc
Akshat Sheth <akshatsheth2310@gmail.com>
2023-05-28 23:19:24 +0530

    useContext added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 // import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import Context from './components/Context';
 import Effect from './components/Effect';
 import UseState from './components/UseState';
 
+export const UserContext = React.createContext()
+
 function App() {
+
   return (
     <div className="App">
-      <UseState />
-      <Effect />
+      <UserContext.Provider value={'user context value'} >
+        <UseState />
+        <Effect />
+        <Context />
+      </UserContext.Provider>
     </div>
   );
-}
+} 
 
 export default App;
